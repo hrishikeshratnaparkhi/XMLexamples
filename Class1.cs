1d2f41301d998495c57a7c975ab3447660ccffc0
Rishi <Rishi@LAPTOP-RI6VRBHC>
2021-08-28 16:50:23 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegateexp
+{
+    class Class1
+    {
+        public delegate void mygenericDelegate<y>(y arg);
+
+        public void genDelMethod()
+        {
+            Console.WriteLine("generic ///////////");
+            mygenericDelegate<string> strTarget = new mygenericDelegate<string>(StingTarget);
+            strTarget("some string data");
+
+            mygenericDelegate<int> intTarget = IntTarget;
+            intTarget(9);
+            Console.ReadLine();
+
+        }
+        public void StringTarget(string arg)
+        {
+            Console.WriteLine("arge in uppercaseis : {0}", arg.ToUpper());
+
+        }
+        public void IntTarget(int arg)
+        {
+            Console.WriteLine("arge in uppercaseis : {0}", ++arg);
+
+        }
+    }
+    class genericDelegateex
+    {
+       public static void Main()
+        {
+            Class1 ob = new Class1();
+            ob.IntTarget(10);
+        }
+    }
+
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DekegateEvent
+{
+    class Multicasteample
+    {
+        public delegate void mydelegate(int i, int j);
+
+        public void Add(int i, int j)
+        {
+            Console.WriteLine(i + j);
+
+        }
+        public void Mul(int i, int j)
+        {
+            Console.WriteLine(i * j);
+
+        }
+        class Mulcastdelegate
+        {
+            public static void Main(string[] args)
+            {
+                Multicasteample me = new Multicasteample();
+                Multicasteample.mydelegate mydelegateintance = null;
+
+                mydelegateintance += new Multicasteample.mydelegate(me.Add);
+                mydelegateintance -= new Multicasteample.mydelegate(me.Mul);
+
+                mydelegateintance(10, 10);
+                mydelegateintance(2, 10);
+
+                
+
+
+            }
+                
+        }
+
+
+
+    }
+    
+}
